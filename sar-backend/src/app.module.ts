00df2c48e9f8174047e77a81d1cd2dd37f267f42
Zach Childers <zach@datumsource.com>
2024-02-17 17:51:24 -0800

    fixing some stuff

--- sar-backend/src/app.module.ts
+++ sar-backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { MemberModule } from './member/member.module';
 import { OperationModule } from './operation/operation.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client'),
+      exclude: ['/sar-backend/*', '/.*'],
+    }),
     MemberModule,
     OperationModule,
     PrismaModule,
